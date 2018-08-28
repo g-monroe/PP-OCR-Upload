ed56aaadc64d148e7a0b0bf890a6e06e52e13576
Gavin M <37052167+g-monroe@users.noreply.github.com>
2018-08-27 19:56:56 -0500

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using ofd As New OpenFileDialog
+            Dim result As DialogResult = ofd.ShowDialog()
+            If result = DialogResult.OK Then
+                Using noel = New NoelShackProvider
+                    Dim response = Await noel.UploadFile(ofd.FileName)
+                    RichTextBox1.Text = response.ToString
+                End Using
+            End If
+        End Using
+    End Sub
+End Class
---
+++ Test.vb
@@ -0,0 +1,29 @@
+﻿'Class:
+Imports System.IO
+Imports System.Net.Http
+Imports System.Text.RegularExpressions
+Public Class NoelShackProvider
+    Implements IDisposable
+    Private _client As HttpClient
+    Sub New()
+        _client = New HttpClient()
+    End Sub
+    Public Async Function UploadFile(filePath As String) As Task(Of String)
+        Try
+            Dim form As New MultipartFormDataContent()
+
+            Dim fBytes As Byte() = File.ReadAllBytes(filePath)
+            Dim fName As String = Path.GetFileName(filePath)
+            form.Add(New ByteArrayContent(fBytes, 0, fBytes.Count()), "file", fName)
+            form.Add(New FormUrlEncodedContent(New Dictionary(Of String, String)() From {{"apikey", "webocr3"}, {"language", "eng"}, {"isOverlayRequired", "true"}}))
+            Dim response = Await _client.PostAsync("https://api.ocr.space/parse/image", form)
+            Return Await response.Content.ReadAsStringAsync()
+        Catch
+            Return "Error!"
+        End Try
+    End Function
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        _client.Dispose()
+    End Sub
+End Class
